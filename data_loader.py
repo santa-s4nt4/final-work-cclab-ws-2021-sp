6f5bea4ca58572a3f1bf4b95ccc3070096153fec
santa <santanaruse@gmail.com>
2021-03-04 01:51:04 +0900

    first commit

---
+++ data_loader.py
@@ -0,0 +1,34 @@
+# %%
+import pandas as pd
+import MeCab
+import time
+
+# %%
+data = pd.read_csv(
+    "/home/santanaruse/dev/PyTorch_workshop_2021/final_work/tweet/tweets.csv", engine="python")
+data["text"] = data["text"].astype(str)
+
+# %%
+data = data[~data["text"].str.contains('@')]
+data = data[~data["text"].str.contains('#')]
+data = data[~data["text"].str.contains('http')]
+data = data[~data["text"].str.contains('RT')]
+
+# %%
+pd.DataFrame(data).to_csv(
+    '/home/santanaruse/dev/PyTorch_workshop_2021/final_work/tweet/tweets_text.txt', index=False, header=None)
+
+# %%
+input_file = '/home/santanaruse/dev/PyTorch_workshop_2021/final_work/tweet/tweets_text.txt'
+
+f = open(input_file, 'r')
+text_data = f.read()
+mecab = MeCab.Tagger("-Owakati")
+text = mecab.parse(text_data)
+mecab.parse('')
+
+timestr = time.strftime('%Y%m%d-%H%M%S')
+
+out_file_name = "/home/santanaruse/dev/PyTorch_workshop_2021/final_work/tweet/wakachi_" + timestr + ".txt"
+with open(out_file_name, 'w') as f:
+    f.write(text)
